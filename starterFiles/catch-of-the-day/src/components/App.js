a841133d4c3ce0f7e05d25c1d087d42f4ec26d9d
Laszlo Bodnar <laszlo.bodnar@c02zd0kglvdq.corp.instructure.com>
2021-01-06 11:11:38 +0100

    video 20 done

--- starterFiles/catch-of-the-day/src/components/App.js
+++ starterFiles/catch-of-the-day/src/components/App.js
@@ -1,78 +1,100 @@
-import React from 'react'
-import base from '../base'
+import React from "react";
+import base from "../base";
 
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
-import Fish from './Fish'
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import Fish from "./Fish";
 
-import sampleFishes from '../sample-fishes'
-
-
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
-    // State
-    state = {
-        fishes: {},
-        order: {}
+  // State
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  // Methods
+  addFish = (fish) => {
+    const fishes = { ...this.state.fishes };
+
+    fishes[`fish${Date.now()}`] = fish;
+
+    this.setState({
+      fishes: fishes,
+    });
+  };
+
+  updateFish = (key, updatedFish) => {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({
+      fishes: sampleFishes,
+    });
+  };
+
+  addToOrder = (key) => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
+
+  // Lifecycle methods
+  componentDidMount() {
+    const { params } = this.props.match;
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if (localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef),
+      });
     }
 
-    // Methods
-    addFish = (fish) => {
-        const fishes = {...this.state.fishes}
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes",
+    });
+  }
 
-        fishes[`fish${Date.now()}`] = fish
+  componentDidUpdate() {
+    const { params } = this.props.match;
+    localStorage.setItem(params.storeId, JSON.stringify(this.state.order));
+  }
 
-        this.setState({
-            fishes: fishes
-        })
-    }
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
 
-    loadSampleFishes = () => {
-        this.setState({
-            fishes: sampleFishes
-        })
-    }
-
-    addToOrder = (key) => {
-        const order = {...this.state.order}
-        order[key] = order[key] + 1 || 1
-        this.setState({order})
-    }
-
-    // Lifecycle methods
-    componentDidMount() {
-        const { params } = this.props.match
-        this.ref = base.syncState(`${params.storeId}/fishes`, {
-            context: this,
-            state: 'fishes'
-        })
-    }
-
-    componentWillUnmount() {
-        console.log(this.ref);
-        base.removeBinding(this.ref)
-    }
-
-    render() {
-        return (
-            <div className="catch-of-the-day">
-                <div className="menu">
-                    <Header tagline="Fresh Seafood Market"/>
-                    <ul className="fishes">
-                        {Object.keys(this.state.fishes).map((key) => (
-                            <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
-                        ))}
-                    </ul>
-                </div>
-                <Order fishes={this.state.fishes} order={this.state.order} />
-                <Inventory 
-                    addFish={this.addFish}
-                    loadSampleFishes={this.loadSampleFishes}
-                />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
+            ))}
+          </ul>
+        </div>
+        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Inventory
+          fishes={this.state.fishes}
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+          updateFish={this.updateFish}
+        />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- starterFiles/catch-of-the-day/src/components/StorePicker.js
+++ starterFiles/catch-of-the-day/src/components/StorePicker.js
@@ -1,33 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import { getFunName } from '../helpers'
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    storeName = React.createRef()
+  storeName = React.createRef();
 
-    goToStore = (e) => {
-        e.preventDefault()
+  goToStore = (e) => {
+    e.preventDefault();
 
-        const storeNameStr = this.storeName.current.value
+    const storeNameStr = this.storeName.current.value;
 
-        this.props.history.push(`/store/${storeNameStr}`)
-    }
+    this.props.history.push(`/store/${storeNameStr}`);
+  };
 
-    render() {
-        return (
-            <form className="store-selector" onSubmit={this.goToStore}>
-                <h2>Please Enter A Store</h2>
-                <input 
-                    type="text"
-                    ref={this.storeName}
-                    placeholder="Store Name" 
-                    defaultValue={getFunName()} 
-                    required
-                />
-                <button type="submit">Visit Store</button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          ref={this.storeName}
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          required
+        />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
+  }
 }
 
-export default StorePicker
+export default StorePicker;
